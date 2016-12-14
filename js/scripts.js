6db3c2810df50229bdcf97fd0a46e9e31c6e9658
Peter Sivilay <sivilaypeter@gmail.com>
2016-12-13 21:18:37 -0600

    Refactor forEach with for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    //blanks.forEach(function(blank) {
+      //var userInput = $("#" + blank).val();
+      //$("." + blank).text(userInput);
+    //});
+
+    for (var index = 0; index < blanks.length; index += 1) {
+      var userInput = $("#" + blanks[index]).val();
+      $("." + blanks[index]).text(userInput);
+    }
 
     $("#story").show();
 
